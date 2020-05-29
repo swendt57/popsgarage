70e0f3467e4cbca0132986e01cc84bfec71f5400
Steve Wendt <stevewendt@cox.net>
2020-05-28 20:52:47 -0700

    Was able to remove the Return to Story button in the galleries and use Juicebox's functionality, freeing up some screen real estate.

--- public/assets/galleries/gallery_scripts.js
+++ public/assets/galleries/gallery_scripts.js
@@ -1,12 +1,13 @@
 function goBack() {
-    window.history.back()
+    // window.history.back()
+    window.location = "/livery/chrysler_crown_imperial";
 }
 
 function doLayout() {
     let winHeight, winWidth;
     winHeight = window.innerHeight ? window.innerHeight : $(window).height();
     winWidth = window.innerWidth ? window.innerWidth : $(window).width();
-    $('#juicebox-content').height(parseInt(winHeight) - 37).width(parseInt(winWidth)); //37 pixels prevents the thumbnails from being cut off
+    $('#juicebox-content').height(parseInt(winHeight)).width(parseInt(winWidth));
 }
 
 $(document).ready(function () {
